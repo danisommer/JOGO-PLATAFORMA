87fe5981d053a455787217364c41dd4b70c43390
danisommer <danibee2005@gmail.com>
2024-12-02 21:14:33 -0300

    ubuntu compile

---
+++ game/build/CMakeFiles/JOGO_2D.dir/DependInfo.cmake
@@ -0,0 +1,49 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/danizs/Programacao/JOGO-PLATAFORMA/game/JOGO_2D/Sources/Animacao.cpp" "CMakeFiles/JOGO_2D.dir/JOGO_2D/Sources/Animacao.cpp.o" "gcc" "CMakeFiles/JOGO_2D.dir/JOGO_2D/Sources/Animacao.cpp.o.d"
+  "/home/danizs/Programacao/JOGO-PLATAFORMA/game/JOGO_2D/Sources/Chefao.cpp" "CMakeFiles/JOGO_2D.dir/JOGO_2D/Sources/Chefao.cpp.o" "gcc" "CMakeFiles/JOGO_2D.dir/JOGO_2D/Sources/Chefao.cpp.o.d"
+  "/home/danizs/Programacao/JOGO-PLATAFORMA/game/JOGO_2D/Sources/Cogumelo.cpp" "CMakeFiles/JOGO_2D.dir/JOGO_2D/Sources/Cogumelo.cpp.o" "gcc" "CMakeFiles/JOGO_2D.dir/JOGO_2D/Sources/Cogumelo.cpp.o.d"
+  "/home/danizs/Programacao/JOGO-PLATAFORMA/game/JOGO_2D/Sources/Ente.cpp" "CMakeFiles/JOGO_2D.dir/JOGO_2D/Sources/Ente.cpp.o" "gcc" "CMakeFiles/JOGO_2D.dir/JOGO_2D/Sources/Ente.cpp.o.d"
+  "/home/danizs/Programacao/JOGO-PLATAFORMA/game/JOGO_2D/Sources/Entidade.cpp" "CMakeFiles/JOGO_2D.dir/JOGO_2D/Sources/Entidade.cpp.o" "gcc" "CMakeFiles/JOGO_2D.dir/JOGO_2D/Sources/Entidade.cpp.o.d"
+  "/home/danizs/Programacao/JOGO-PLATAFORMA/game/JOGO_2D/Sources/Espinho.cpp" "CMakeFiles/JOGO_2D.dir/JOGO_2D/Sources/Espinho.cpp.o" "gcc" "CMakeFiles/JOGO_2D.dir/JOGO_2D/Sources/Espinho.cpp.o.d"
+  "/home/danizs/Programacao/JOGO-PLATAFORMA/game/JOGO_2D/Sources/Fase.cpp" "CMakeFiles/JOGO_2D.dir/JOGO_2D/Sources/Fase.cpp.o" "gcc" "CMakeFiles/JOGO_2D.dir/JOGO_2D/Sources/Fase.cpp.o.d"
+  "/home/danizs/Programacao/JOGO-PLATAFORMA/game/JOGO_2D/Sources/Gerenciador_Colisoes.cpp" "CMakeFiles/JOGO_2D.dir/JOGO_2D/Sources/Gerenciador_Colisoes.cpp.o" "gcc" "CMakeFiles/JOGO_2D.dir/JOGO_2D/Sources/Gerenciador_Colisoes.cpp.o.d"
+  "/home/danizs/Programacao/JOGO-PLATAFORMA/game/JOGO_2D/Sources/Gerenciador_Eventos.cpp" "CMakeFiles/JOGO_2D.dir/JOGO_2D/Sources/Gerenciador_Eventos.cpp.o" "gcc" "CMakeFiles/JOGO_2D.dir/JOGO_2D/Sources/Gerenciador_Eventos.cpp.o.d"
+  "/home/danizs/Programacao/JOGO-PLATAFORMA/game/JOGO_2D/Sources/Gerenciador_Grafico.cpp" "CMakeFiles/JOGO_2D.dir/JOGO_2D/Sources/Gerenciador_Grafico.cpp.o" "gcc" "CMakeFiles/JOGO_2D.dir/JOGO_2D/Sources/Gerenciador_Grafico.cpp.o.d"
+  "/home/danizs/Programacao/JOGO-PLATAFORMA/game/JOGO_2D/Sources/Inimigo.cpp" "CMakeFiles/JOGO_2D.dir/JOGO_2D/Sources/Inimigo.cpp.o" "gcc" "CMakeFiles/JOGO_2D.dir/JOGO_2D/Sources/Inimigo.cpp.o.d"
+  "/home/danizs/Programacao/JOGO-PLATAFORMA/game/JOGO_2D/Sources/Jogador.cpp" "CMakeFiles/JOGO_2D.dir/JOGO_2D/Sources/Jogador.cpp.o" "gcc" "CMakeFiles/JOGO_2D.dir/JOGO_2D/Sources/Jogador.cpp.o.d"
+  "/home/danizs/Programacao/JOGO-PLATAFORMA/game/JOGO_2D/Sources/Lista.cpp" "CMakeFiles/JOGO_2D.dir/JOGO_2D/Sources/Lista.cpp.o" "gcc" "CMakeFiles/JOGO_2D.dir/JOGO_2D/Sources/Lista.cpp.o.d"
+  "/home/danizs/Programacao/JOGO-PLATAFORMA/game/JOGO_2D/Sources/ListaEntidade.cpp" "CMakeFiles/JOGO_2D.dir/JOGO_2D/Sources/ListaEntidade.cpp.o" "gcc" "CMakeFiles/JOGO_2D.dir/JOGO_2D/Sources/ListaEntidade.cpp.o.d"
+  "/home/danizs/Programacao/JOGO-PLATAFORMA/game/JOGO_2D/Sources/Menu.cpp" "CMakeFiles/JOGO_2D.dir/JOGO_2D/Sources/Menu.cpp.o" "gcc" "CMakeFiles/JOGO_2D.dir/JOGO_2D/Sources/Menu.cpp.o.d"
+  "/home/danizs/Programacao/JOGO-PLATAFORMA/game/JOGO_2D/Sources/Obstaculo.cpp" "CMakeFiles/JOGO_2D.dir/JOGO_2D/Sources/Obstaculo.cpp.o" "gcc" "CMakeFiles/JOGO_2D.dir/JOGO_2D/Sources/Obstaculo.cpp.o.d"
+  "/home/danizs/Programacao/JOGO-PLATAFORMA/game/JOGO_2D/Sources/OlhoVoador.cpp" "CMakeFiles/JOGO_2D.dir/JOGO_2D/Sources/OlhoVoador.cpp.o" "gcc" "CMakeFiles/JOGO_2D.dir/JOGO_2D/Sources/OlhoVoador.cpp.o.d"
+  "/home/danizs/Programacao/JOGO-PLATAFORMA/game/JOGO_2D/Sources/Parede.cpp" "CMakeFiles/JOGO_2D.dir/JOGO_2D/Sources/Parede.cpp.o" "gcc" "CMakeFiles/JOGO_2D.dir/JOGO_2D/Sources/Parede.cpp.o.d"
+  "/home/danizs/Programacao/JOGO-PLATAFORMA/game/JOGO_2D/Sources/Personagem.cpp" "CMakeFiles/JOGO_2D.dir/JOGO_2D/Sources/Personagem.cpp.o" "gcc" "CMakeFiles/JOGO_2D.dir/JOGO_2D/Sources/Personagem.cpp.o.d"
+  "/home/danizs/Programacao/JOGO-PLATAFORMA/game/JOGO_2D/Sources/Plataforma.cpp" "CMakeFiles/JOGO_2D.dir/JOGO_2D/Sources/Plataforma.cpp.o" "gcc" "CMakeFiles/JOGO_2D.dir/JOGO_2D/Sources/Plataforma.cpp.o.d"
+  "/home/danizs/Programacao/JOGO-PLATAFORMA/game/JOGO_2D/Sources/Portal.cpp" "CMakeFiles/JOGO_2D.dir/JOGO_2D/Sources/Portal.cpp.o" "gcc" "CMakeFiles/JOGO_2D.dir/JOGO_2D/Sources/Portal.cpp.o.d"
+  "/home/danizs/Programacao/JOGO-PLATAFORMA/game/JOGO_2D/Sources/Principal.cpp" "CMakeFiles/JOGO_2D.dir/JOGO_2D/Sources/Principal.cpp.o" "gcc" "CMakeFiles/JOGO_2D.dir/JOGO_2D/Sources/Principal.cpp.o.d"
+  "/home/danizs/Programacao/JOGO-PLATAFORMA/game/JOGO_2D/Sources/Projetil.cpp" "CMakeFiles/JOGO_2D.dir/JOGO_2D/Sources/Projetil.cpp.o" "gcc" "CMakeFiles/JOGO_2D.dir/JOGO_2D/Sources/Projetil.cpp.o.d"
+  "/home/danizs/Programacao/JOGO-PLATAFORMA/game/JOGO_2D/Sources/Serra.cpp" "CMakeFiles/JOGO_2D.dir/JOGO_2D/Sources/Serra.cpp.o" "gcc" "CMakeFiles/JOGO_2D.dir/JOGO_2D/Sources/Serra.cpp.o.d"
+  "/home/danizs/Programacao/JOGO-PLATAFORMA/game/JOGO_2D/Sources/Slime.cpp" "CMakeFiles/JOGO_2D.dir/JOGO_2D/Sources/Slime.cpp.o" "gcc" "CMakeFiles/JOGO_2D.dir/JOGO_2D/Sources/Slime.cpp.o.d"
+  "/home/danizs/Programacao/JOGO-PLATAFORMA/game/JOGO_2D/Sources/Tela.cpp" "CMakeFiles/JOGO_2D.dir/JOGO_2D/Sources/Tela.cpp.o" "gcc" "CMakeFiles/JOGO_2D.dir/JOGO_2D/Sources/Tela.cpp.o.d"
+  "/home/danizs/Programacao/JOGO-PLATAFORMA/game/JOGO_2D/Sources/main.cpp" "CMakeFiles/JOGO_2D.dir/JOGO_2D/Sources/main.cpp.o" "gcc" "CMakeFiles/JOGO_2D.dir/JOGO_2D/Sources/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
